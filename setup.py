8039b77471d419b0d4520dd155f76e1cb77637e6
Tucker Joseph <tucker@chartboost.com>
2014-07-23 12:13:02 -0700

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 import sys
 
 setup(
-    name = "dogstatsd-python",
-    version = "0.5.1",
-    author = "Datadog, Inc.",
-    author_email = "packages@datadoghq.com",
-    description = "Python bindings to Datadog's API and a user-facing command line tool.",
-    py_modules=['statsd'],
-    license = "BSD",
-    keywords = "datadog data statsd metrics",
-    url = "http://www.datadoghq.com"
+    name="dogstatsd-python",
+    version="0.5.1",
+    author="Chartboost, Inc.",
+    author_email="packages@datadoghq.com",
+    description="Tornado Python bindings to Datadog's API and a user-facing command line tool.",
+    py_modules=['statsd_tornado'],
+    license="BSD",
+    keywords="datadog data statsd metrics",
+    url="http://www.datadoghq.com"
 )
